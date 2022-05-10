084494af32fbd810c5a41016e9a1df233c39b363
konstantin.tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2022-05-10 13:26:20 +0200

    Update Kotlin versions.

--- src/commonMain/kotlin/org/jetbrains/webwiz/models/KotlinVersions.kt
+++ src/commonMain/kotlin/org/jetbrains/webwiz/models/KotlinVersions.kt
@@ -1,7 +1,7 @@
 package org.jetbrains.webwiz.models
 
 enum class KotlinVersion(val versionName: String) {
-    Stable("1.6.20"),
-    EAP("1.6.20"),
+    Stable("1.6.21"),
+    EAP("1.7.0-Beta"),
     Dev("1.7.0")
 }
