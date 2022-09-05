e3b74f2fc93e15e3768e0c781d2d203dccbe8d91
konstantin.tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2022-09-05 17:46:15 +0200

    Update project dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,14 @@ import org.jetbrains.compose.compose
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
 
 plugins {
-    kotlin("multiplatform") version "1.6.10"
-    id("org.jetbrains.compose") version "1.1.1"
+    kotlin("multiplatform") version "1.7.10"
+    id("org.jetbrains.compose") version "1.2.0-alpha01-dev774"
 }
 
 repositories {
     mavenCentral()
     google()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 kotlin {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 rootProject.name = "kmp-web-wizard"
